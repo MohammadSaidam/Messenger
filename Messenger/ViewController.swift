a1d3f6dd42446019c23677a85a033880505ecd41
saidam2002 <msaidam58@gmail.com>
2023-01-22 13:18:43 +0200

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Mohammed Saidam on 22/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
